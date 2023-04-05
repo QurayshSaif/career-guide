98273d9c5ccddf0ad74b0d26078ae4b0b00bce0d
QurayshSaif <saifquraysh@gmaill.com>
2023-04-05 13:35:49 -0400

    display questions from api and gives top 3 results

--- src/components/CareerTest/CareerTest.jsx
+++ src/components/CareerTest/CareerTest.jsx
@@ -6,6 +6,9 @@ import { API_URL } from "../../utils/api";
 export default function CareerTest() {
   const [quiz, setQuiz] = useState([]);
   const [scoringSystem, setScoringSystem] = useState({});
+  const [answers, setAnswers] = useState([]);
+  const [scores, setScores] = useState({});
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   useEffect(() => {
     axios
@@ -26,6 +29,108 @@ export default function CareerTest() {
       })
       .catch((error) => console.log(error));
   }, []);
+  const handleAnswerSelect = (answerValue) => {
+    const updatedAnswers = [...answers];
+    updatedAnswers[currentQuestionIndex] = answerValue;
+    setAnswers(updatedAnswers);
+  };
 
-  return <div className="quiz"></div>;
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  const handlePreviousQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex - 1);
+  };
+
+  const calculateScores = () => {
+    const updatedScores = {};
+
+    answers.forEach((answer) => {
+      if (answer) {
+        const answerScore = scoringSystem[answer];
+        Object.keys(answerScore).forEach((field) => {
+          updatedScores[field] =
+            (updatedScores[field] || 0) + answerScore[field];
+        });
+      }
+    });
+
+    setScores(updatedScores);
+    console.log(updatedScores);
+  };
+
+  const getTopFields = () => {
+    const sortedFields = Object.entries(scores)
+      .sort(([, scoreA], [, scoreB]) => scoreB - scoreA)
+      .map(([field]) => field);
+    return sortedFields.slice(0, 3);
+  };
+
+  const currentQuestion = quiz[currentQuestionIndex];
+
+  return (
+    <div className="quiz">
+      {quiz.length > 0 ? (
+        <>
+          <p className="quiz__question">{currentQuestion.question}</p>
+          {currentQuestion.answers.map((answer) => (
+            <label className="quiz__label" key={answer.value}>
+              <input
+                className="quiz__input"
+                type="radio"
+                name={`question-${currentQuestionIndex}`}
+                value={answer.value}
+                onChange={() => handleAnswerSelect(answer.value)}
+                checked={answers[currentQuestionIndex] === answer.value}
+              />
+              <span className="quiz__text">{answer.text}</span>
+            </label>
+          ))}
+          <div className="quiz__buttons">
+            {currentQuestionIndex > 0 && (
+              <div
+                className="quiz__button quiz__button--previous"
+                onClick={handlePreviousQuestion}
+              >
+                Previous Question
+              </div>
+            )}
+            {currentQuestionIndex < quiz.length - 1 && (
+              <div
+                className="quiz__button quiz__button--next"
+                onClick={handleNextQuestion}
+              >
+                Next Question
+              </div>
+            )}
+            {currentQuestionIndex === quiz.length - 1 && (
+              <div
+                className="quiz__button quiz__button--submit"
+                onClick={calculateScores}
+              >
+                Submit
+              </div>
+            )}
+          </div>
+          {Object.keys(scores).length > 0 && (
+            <div className="quiz__result">
+              <p className="quiz__result-text">
+                Top fields based on your answers:
+              </p>
+              <ul className="quiz__result-list">
+                {getTopFields().map((field) => (
+                  <li className="quiz__result-item" key={field}>
+                    {field}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
